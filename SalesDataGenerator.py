95a9c9a253db7a873e4b6f2b56971a07fe9b0b08
Hazel Dow <hazelmadefresh@gmail.com>
2024-04-10 22:04:25 -0500

    Finished the first iteration of the init function for customers, completed the majority of the init function for products, and now need to complete the CategoryGenerator init function.

--- SalesDataGenerator.py
+++ SalesDataGenerator.py
@@ -22,9 +22,7 @@ def read_template(name):
         return columns
 
 
-prod_columns = ["Item ID", "Wholesale Price", "Retail Price", "Num in Inventory", "Total Sold", "Current Discount",
-                "Expiration Date", "Category"]
-cust_columns = ["Customer ID", "Experience", "Starting Cash", "Spending Cash"]
+cust_columns = ["Customer ID", "Experience", "Starting Cash", "Spending Cash", "Categories"]
 sale_columns = ["Item(s)", "Wholesale Price(s)", "Retail Price(s)", "Discount(s)", "Number Sold(each)", "Timestamp",
                 "Expiration Date(s)"]
 category_columns = ["Category ID", "Intensity"]
@@ -42,6 +40,17 @@ def compress_int(number, min_n, max_n):
         return (min_n + max_n) / 2
 
 
+class CategoryGenerator:
+    __generatortype__ = DataGeneratorType.CATEGORY
+    MAX_CATEGORIES = 100
+    MIN_CATEGORIES = 10
+
+    def __init__(self, categories, avg_intensity):
+        self.df = pd.DataFrame(columns=category_columns)
+        self.df['Category ID'] = pd.Series([i for i in range(1, categories + 1)])
+        self.df['Intensity'] = pd.Series(np.random.normal(loc=avg_intensity))
+
+
 class CustomerGenerator:
     __generatortype__ = DataGeneratorType.CUSTOMER
 
@@ -51,7 +60,8 @@ class CustomerGenerator:
     MIN_SPENDING_CASH = 5
     SCALE = 0.114
 
-    def __init__(self, customers, avg_cash, min_cash, max_cash, average_experience, perc_low, perc_avg, perc_high):
+    def __init__(self, categories: CategoryGenerator, customers, avg_cash, min_cash, max_cash, average_experience,
+                 perc_low, perc_avg, perc_high):
         # Validate the requested price distribution
         if not perc_low + perc_avg + perc_high == 1.0:
             raise ValueError("Invalid Cash Distribution")
@@ -78,22 +88,36 @@ class CustomerGenerator:
                 n_avg -= 1
             tot = n_low + n_avg + n_high
 
+        rng = np.random.default_rng()
         # Generate that number of customer uuids
         uuids = pd.Series([i for i in range(0, customers)])
         # And starting cashes
-        low = np.random.normal(loc=min_cash, scale=min_cash / 2, size=n_low).tolist()
-        avg = np.random.normal(loc=avg_cash, scale=avg_cash / 2, size=n_avg).tolist()
-        high = np.random.normal(loc=max_cash, scale=max_cash / 2, size=n_high).tolist()
+        low = rng.normal(loc=min_cash, scale=min_cash / 2, size=n_low).tolist()
+        avg = rng.normal(loc=avg_cash, scale=avg_cash / 2, size=n_avg).tolist()
+        high = rng.normal(loc=max_cash, scale=max_cash / 2, size=n_high).tolist()
         cashes = pd.Series(np.append(low, [avg, high]))
         # As well as starting experience level
         experiences = pd.Series(
-            np.random.normal(loc=average_experience, scale=CustomerGenerator.SCALE, size=customers))
+            rng.normal(loc=average_experience, scale=CustomerGenerator.SCALE, size=customers))
+
+        # Now we give them "interests"
+        ints_per_each = rng.integers(low=1, high=21, size=customers)
+        interests = []
+        num_categories = categories.df.shape[0]
+        for customer in range(0, customers):
+            interests[customer] = rng.integers(low=0, high=num_categories + 1, size=ints_per_each[customer])
+        interests = pd.Series()
 
         # Then add the values to our DataFrame
         self.df['Customer ID'] = uuids
         self.df['Experience'] = experiences
         self.df['Starting Cash'] = cashes
         self.df['Spending Cash'] = cashes
+        self.df["Categories"] = interests
+
+
+prod_columns = ["Item ID", "Wholesale Price", "Retail Price", "Num in Inventory", "Total Sold", "Current Discount",
+                "Expiration Date", "Categories"]
 
 
 class ProductsGenerator:
@@ -103,10 +127,18 @@ class ProductsGenerator:
     MAX_PRICE = 500
     MIN_PRICE = 1
     MAX_CATEGORIES = 100
+    MAX_WHOLESALE = 100
+    MARKUP = 4
+    MAX_INVENTORY = 10
 
-    def __init__(self, rows: int, categories: int, min_price: float, avg_price: float, max_price: float, perc_low,
+    def __init__(self, products: int, categories: CategoryGenerator,
+                 min_price: float,
+                 avg_price: float,
+                 max_price: float,
+                 perc_low,
                  perc_avg,
                  perc_high):
+        rng = np.random.default_rng()
         # Validate the requested price ranges
         if not min_price < avg_price < max_price:
             raise ValueError("Invalid Price Range")
@@ -114,24 +146,32 @@ class ProductsGenerator:
         if not perc_low + perc_avg + perc_high == 1.0:
             raise ValueError("Invalid Price Distribution")
         # Compress the requested row count
-        rows = compress_int(rows, ProductsGenerator.MIN_ROWS, ProductsGenerator.MAX_ROWS)
+        products = compress_int(products, ProductsGenerator.MIN_ROWS, ProductsGenerator.MAX_ROWS)
         # And the prices requested
         min_price = compress_int(min_price, ProductsGenerator.MIN_PRICE, avg_price)
         max_price = compress_int(max_price, avg_price, ProductsGenerator.MAX_PRICE)
         # Create the DataFrame needed
         self.df = pd.DataFrame(columns=prod_columns)
-        # Then generate the product categories
-
-
-class CategoryGenerator:
-    __generatortype__ = DataGeneratorType.CATEGORY
-    MAX_CATEGORIES = 100
-    MIN_CATEGORIES = 10
-
-    def __init__(self, categories, avg_intensity):
-        self.df = pd.DataFrame(columns=category_columns)
-        self.df['Category ID'] = pd.Series([i for i in range(1, categories + 1)])
-        self.df['Intensity'] = pd.Series(np.random.normal(loc=avg_intensity))
+        # Then generate the product uuids
+        uuids = pd.Series([i for i in range(0, products)])
+        # And the wholesale prices
+        wholesale = pd.Series(rng.random(size=products)) * ProductsGenerator.MAX_WHOLESALE
+        # Multiply by the markup to get retail price
+        retail = wholesale * ProductsGenerator.MARKUP
+        # Randomly generate inventory numbers
+        inventory = pd.Series(rng.integers(InterestsGlow=1, high=ProductsGenerator.MAX_INVENTORY, size=products))
+        # Set sales to 0
+        sales = pd.Series(np.zeros(products))
+        # Giving ten percent of items a 25% or 50% discount
+        discounts = pd.Series([rng.choice([.25, .50]) for i in rng.random(size=products) if i < 0.1])
+        # Give expiration date in timestamp one to two years from now
+        year_secs = 31536000
+        expirations = ((pd.Series(rng.random(size=products)) + 1) * year_secs) + datetime.datetime.now().timestamp()
+        # Select some categories for each product, 1-3
+        # TODO: You MUST implement the CategoryGenerator before continuing here.
+        #       The usage of the uuid of the categories given will currently result in a runtime error.
+        cats = [rng.choice(categories.df.shape[0] + 1, size=i) for i in
+                pd.Series(rng.integers(low=1, high=4, size=products))]
 
 
 class SalesDataGenerator:
@@ -143,7 +183,7 @@ class SalesDataGenerator:
 
         if timestamp is None:
             # Setting timestamp to current time if none given
-            self.begin = datetime.datetime.now()
+            self.begin = datetime.datetime.now().timestamp()
         else:
             # Checking if within 1970-2038 (Epochalypse)
             if not isinstance(timestamp, int) or not (-1 < timestamp < 2145938400):
---
+++ anonymizationtest.py
@@ -0,0 +1,4 @@
+import torch
+
+prod_columns = ["ID", "price", "retail", "stock"]
+sale_columns = ["ID", "timestamp", ""]
